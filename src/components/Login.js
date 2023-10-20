b0a6d3b91963e057996615eea63ce9d3c52ae000
surajthakkar7 <47253144+surajthakkar7@users.noreply.github.com>
2023-10-20 21:26:09 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ const Login = (props) => {
     e.preventDefault();
 
     try {
-      const response = await fetch(`http://localhost:5000/api/auth/login`, {
+      const response = await fetch(`https://securescriptbackend.onrender.com/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
